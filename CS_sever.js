02b2ddd9dadb23e7dbc25e16683db899ab019326
kenny_C_S <kennyliao369@gmail.com>
2025-04-24 17:38:41 +0800

    2025/04/24

--- CS_sever.js
+++ CS_sever.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 // 假設這是你要提供的資料
 const userData = {
@@ -12,6 +12,10 @@ app.get("/api/:id", (req, res) => {
   const id = req.params.id;
   const data = userData[id] || { error: "Not found" };
   res.json(data);
+});
+
+app.get('/', (req, res) => {
+  res.send('伺服器已上線！');
 });
 
 app.listen(PORT, () => {
